6e310c35cd3a753bd15fb8fa6e663ab59aeef451
paconut <paconut1@gmail.com>
2024-02-08 13:27:56 -0600

    se hizo el ejercicio 4

---
+++ liveCoding4.java
@@ -0,0 +1,13 @@
+package com.generation;
+
+import java.util.Scanner;
+
+public class liveCoding4 {
+
+    public static void main(String[] args) {
+
+        FizzBuzz calculo = new FizzBuzz();
+        calculo.printFizzBuzz();
+    }
+
+}
